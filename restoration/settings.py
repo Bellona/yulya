5000a4f108c37177219eef1d76ada2408000fbbe
Marina <marina@ubuntu.(none)>
2013-12-19 19:36:06 -0800

    static

--- restoration/settings.py
+++ restoration/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -81,3 +81,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'yulya', 'static')
--- restoration/urls.py
+++ restoration/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,4 +14,4 @@ urlpatterns = patterns('',
 	url(r'^contacts/', 'yulya.views.contacts', name='contacts'),
 
 	url(r'^admin/', include(admin.site.urls)),
-)
+) + static('/static/', document_root=settings.STATIC_ROOT)
