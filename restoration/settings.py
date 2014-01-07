4644505da0f6f66eda74211589fd58f43a9330bc
Marina Donskikh <bellon@mail.ru>
2014-01-06 18:19:52 -0800

    a

--- restoration/settings.py
+++ restoration/settings.py
@@ -57,14 +57,14 @@ WSGI_APPLICATION = 'restoration.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
+}
+
+if not DEBUG:
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
 
